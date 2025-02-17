f9a5a35e8d0b39f2848b927241984bf682eeb76f
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-02-17 23:40:34 +0100

    Refactor SearchForm to use Input component instead of SidebarInput

--- src/components/common/search-form.tsx
+++ src/components/common/search-form.tsx
@@ -1,11 +1,7 @@
 import { Search } from "lucide-react";
 
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarInput,
-} from "@/components/ui/sidebar";
 
 type Props = {
   handleSearch?: (value: string) => void;
@@ -14,20 +10,18 @@ type Props = {
 export function SearchForm({ handleSearch }: Props) {
   return (
     <form className="w-1/2 mx-auto">
-      <SidebarGroup className="p-0">
-        <SidebarGroupContent className="relative">
-          <Label htmlFor="search" className="sr-only">
-            Search
-          </Label>
-          <SidebarInput
-            id="search"
-            className="pl-8"
-            placeholder="Search pokemon..."
-            onChange={(e) => handleSearch?.(e.target.value)}
-          />
-          <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
-        </SidebarGroupContent>
-      </SidebarGroup>
+      <div className="relative">
+        <Label htmlFor="search" className="sr-only">
+          Search
+        </Label>
+        <Input
+          id="search"
+          className="pl-8"
+          placeholder="Search pokemon..."
+          onChange={(e) => handleSearch?.(e.target.value)}
+        />
+        <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+      </div>
     </form>
   );
 }
